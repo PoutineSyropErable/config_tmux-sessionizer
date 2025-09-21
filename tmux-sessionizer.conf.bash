1d36f2a63f64b08aa956a4f278f5e08650b7e3a8
François Pagé <francois.page@mail.mcgill.ca>
2025-09-21 19:47:03 -0400

    nvim submodules added. possession, meld-like merge.

--- tmux-sessionizer.conf.bash
+++ tmux-sessionizer.conf.bash
@@ -1,18 +1,23 @@
 # file: ~/.config/tmux-sessionizer/tmux-sessionizer.conf
-TS_SESSION_COMMANDS=(lf .)
+TS_SESSION_COMMANDS=(ls pwd ./build.sh)
 
 # Replace defaults completely
 TS_SEARCH_PATHS=(
 	# "$HOME/.config/"
 	"$HOME/.config/lf"
+	"$HOME/.config/hypr"
+
 	"$HOME/.config/nvim"
+	"$HOME/.config/nvim/meld_like_merge"
+	"$HOME/.config/nvim/nvim-possession"
+	"$HOME/.config/nvim/scripts/pythonScripts"
+
 	"$HOME/.config/tmux"
 	"$HOME/.config/zsh"
 	"$HOME/.config/tmux-sessionizer-config"
 	"$HOME/.local/tmux-sessionizer-local"
 	"$HOME/Documents/University (Real)/Semester 10/Comp 303/AutomakeJava"
 	"$HOME/Documents/University (Real)/Semester 10/Comp 303/Project/Comp303Project"
-	"$HOME/.config/nvim/scripts/pythonScripts"
 	"$HOME/Documents/zzz__PersonalProjects/MapleKernel"
 	"$HOME/Documents/zzz__PersonalProjects/MapleKernel/src/"
 	"$HOME/Documents/zzz__PersonalProjects/MapleKernel/Tutorials/meaty-skeleton/"
@@ -23,9 +28,10 @@ TS_SEARCH_PATHS=(
 
 # Optionally add more paths
 TS_EXTRA_SEARCH_PATHS=(
+	"$HOME/Documents/zzz__PersonalProjects/Learn C And Assembly/modern-c textbook_example_code"
+
 	"$HOME/Documents/zzz__PersonalProjects/Learn C And Assembly/Exercises/:2"
 	"$HOME/Documents/zzz__PersonalProjects/Learn C And Assembly/arch1001_x86-64_asm_code_for_class:0"
-	"$HOME/Documents/zzz__PersonalProjects/Learn C And Assembly/modern-c textbook example code:0"
 	"$HOME/Documents/zzz__PersonalProjects/Learn C And Assembly/arch1001_x86-64_asm_slides_and_subtitles:0"
 )
 
